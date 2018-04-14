23e66017d507984cd6de6febb143d72b97443a73
MorganArthox <morgan.chetelat@outlook.com>
2018-04-14 14:23:12 +0200

    Avancement

--- src/app.js
+++ src/app.js
@@ -13,20 +13,26 @@ let game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('background', 'src/games/firstgame/assets/sky.png');
+    this.load.image('background', 'img/herbe.png');
+
    /* this.load.image('ground', 'src/games/firstgame/assets/platform.png');
     this.load.image('star', 'src/games/firstgame/assets/star.png');
-    this.load.image('bomb', 'src/games/firstgame/assets/bomb.png');*/
+    this.load.image('bomb', 'src/games/firstgame/assets/bomb.png');
     this.load.spritesheet('dude',
         'src/games/firstgame/assets/dude.png',
         { frameWidth: 32, frameHeight: 48 }
-    );
-}
+    );*/
+}this.bg= this.game.add.tileSprite(0, 0, game.stage.bounds.width, game.cache.getImage('background').height, 'background');
+// Display the bird on the screen
 
 function create () {
-    this.add.image(400, 300, 'background');
+    //this.add.image(400, 300, 'background');
+    this.bg= this.game.add.tileSprite(0, 0, game.stage.bounds.width, game.cache.getImage('background').height, 'background');
+}
+0, 300, 'background');
 }
 
 function update ()
 {
 }
+
