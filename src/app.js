f3b5ee96a491cb693c6db578410acd5aea7d1beb
MorganArthox <morgan.chetelat@outlook.com>
2018-04-14 17:07:12 +0200

    Dernier commit de la journée

--- src/app.js
+++ src/app.js
@@ -10,37 +10,57 @@ let config = {
 };
 
 let game = new Phaser.Game(config);
+let cursors = game.input.keyboard.createCursorKeys();
+let bonobo;
 
 function preload ()
 {
-    this.load.image('three', 'img/Image_Flat/Grass/Grass_Three.png');
+    this.load.image('bonobo', 'img/Image_Flat/Personnages/Bonobo/Bonobo.png');
+    this.load.image('three', 'img/Image_Flat/Grass/Grass_Tree.png');
     this.load.image('grass-simple', 'img/Image_Flat/Grass/Grass_Simple.png');
-    this.load.image('player', 'img/Image_Flat/Personnages/Bonobo/BONOBO.png');
     this.load.image('grass-puddle-small', 'img/Image_Flat/Grass/Grass_puddle_small.png');
+    this.load.image('grass-puddle-medium', 'img/Image_Flat/Grass/Grass_puddle_medium.png');
+    this.load.image('grass-puddle-large', 'img/Image_Flat/Grass/Grass_puddle_large.png');
+    this.load.image('grass-bush-medium', 'img/Image_Flat/Grass/Grass_bush_medium.png');
+    this.load.image('grass-bush-small', 'img/Image_Flat/Grass/Grass_bush_small.png');
+    this.load.image('grass-bush-large', 'img/Image_Flat/Grass/Grass_bush_large.png');
+    this.load.image('grass-stone-large', 'img/Image_Flat/Grass/Grass_stone_large.png');
+    this.load.image('grass-stone-medium', 'img/Image_Flat/Grass/Grass_stone_medium.png');
+    this.load.image('grass-stone-small', 'img/Image_Flat/Grass/Grass_stone_small.png');
+
 }
 
 function create () {
+
     this.add.image(64,64,'three');
-    this.add.image(64,64,'player').setScale(0.125);
-    this.add.image(191,64,'grass-simple');
-    this.add.image(318,64,'grass-puddle-small')
-        /*let player = game.add.sprite(32, game.world.height - 150, 'player');
+    this.add.image(192,64,'grass-simple');
+    this.add.image(320,64,'grass-puddle-small');
+    bonobo = this.add.sprite(64, 64, 'bonobo').setScale(0.125);
+    /*game.physics.arcade.enable(bonobo);*/
 
-    //  We need to enable physics on the player
-    game.physics.arcade.enable(player);
 
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
-    player.body.collideWorldBounds = true;
-
-    //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);*/
 
 }
 
 
-function update ()
-{
+function update (){
+    let cursors = game.input.keyboard.createCursorKeys();
+
+    if (cursors.left.isDown){
+        bonobo.setPosition(64,0,0,0)
+    }
+
+    else if (cursors.right.isDown){
+        bonobo.setPosition(-64,0,0,0);
+    }
+
+    else {
+        bonobo.setVelocityX(0);
+    }
+    if (cursors.up.isDown && bonobo.body.touching.down) {
+        bonobo.setVelocityY(-330);
+    }
 }
+
+
+
